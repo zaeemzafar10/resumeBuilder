9b23673ae864b0d3bbdeab68ffaa9c45cd0bbd1e
zaeemzafar10 <zaeem.zafar@salsoft.net>
2025-05-12 17:11:45 +0500

    changes involveds

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -17,7 +17,6 @@ if (process.env.NODE_ENV !== "production" && process.env.REPL_ID !== undefined) 
 }
 
 export default defineConfig({
-  root: path.resolve(__dirname, "client"),
   plugins: [
     react(),
     ...replitPlugins
@@ -29,13 +28,12 @@ export default defineConfig({
       "@assets": path.resolve("../attached_assets"),
     },
   },
-  // server: {
-  //   host: "0.0.0.0",
-  //   port: 3000
-  // },
+  server: {
+    host: "0.0.0.0",
+    port: 3000
+  },
   build: {
     outDir: "../dist",
     emptyOutDir: true,
   },
-  base: './',
 });
